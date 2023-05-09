a76b9c6a78d831011864f6c142e51d9621344a8d
ziomek64 <mradymski3@gmail.com>
2023-05-09 17:33:16 +0200

    Real World scenario

---
+++ TabBarPhotoTest/ViewModel/NewPage1ViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TabBarPhotoTest.ViewModel
+{
+    class NewPage1ViewModel : INotifyPropertyChanged
+    {
+
+
+        public NewPage1ViewModel()
+        {
+            RefreshCommand = new Command(async () => await Refresh());
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        double fontSize = 14;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ICommand RefreshCommand { get; }
+        public bool isBusy = false;
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (value == isBusy)
+                    return;
+
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public string _avatar;
+
+        public string Avatar
+        {
+            get => _avatar;
+            set
+            {
+                if (_avatar == value)
+                    return;
+
+                _avatar = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string _banner;
+
+        public string Banner
+        {
+            get => _banner;
+            set
+            {
+                if (_banner == value)
+                    return;
+
+                _banner = value;
+                OnPropertyChanged();
+            }
+        }
+
+        async Task Refresh()
+        {
+            Avatar = "https://s4.anilist.co/file/anilistcdn/user/avatar/large/b5559348-jekjXFK3LX49.jpg";
+            Banner = "https://s4.anilist.co/file/anilistcdn/user/banner/b5559348-kU5EdNn7M6qW.jpg";
+
+            IsBusy = false;
+        }
+    }
+}
--- TabBarPhotoTest/Views/NewPage1.xaml.cs
+++ TabBarPhotoTest/Views/NewPage1.xaml.cs
@@ -1,9 +1,20 @@
+using Microsoft.Maui.Controls;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TabBarPhotoTest.ViewModel;
+
 namespace TabBarPhotoTest.Views;
 
-public partial class NewPage1 : ContentPage
+public partial class NewPage1
 {
 	public NewPage1()
-	{
-		InitializeComponent();
+    {
+        BindingContext = new NewPage1ViewModel();
+        InitializeComponent();
 	}
+
+    
 }
